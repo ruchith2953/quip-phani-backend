a132e7c60c01823871e18de5b9abd8c168f464d2
Murali-295 <murali.kolli@nextrow.com>
2025-11-20 16:54:47 +0530

    Initial commit

---
+++ src/test/java/com/quip/coa/CoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quip.coa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
